13009286cd392bbd342119e5cc371979d0e102f3
BRYX <graysonantonbryx@gmail.com>
2025-05-23 17:06:36 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -8,6 +8,63 @@ P = '\x1b[1;35m'
 G = '\x1b[1;32m'
 R = '\x1b[1;31m'
 J = '\x1b[38;5;208m'
+purple = "\033[1;35m"
+violet_chu = "\033[1;35m"
+darkblue = "\033[34m"
+green1 = "\033[1;32m"
+green2 = "\x1b[38;5;46m"
+red = "\033[1;31m"
+yellow = "\033[1;33m"
+skyblue = "\033[1;36m"
+blue = "\033[1;34m"
+lightblue = "\033[38;5;81m"
+hotpink = "\033[38;5;197m"
+light_magenta = "\033[38;5;174m"
+white = "\033[1;37m"
+lavender = "\033[38;5;189m"
+rasp = "\033[38;5;22m"
+darkblue = "\033[34m"
+green = "\033[1;32m"
+red = "\033[1;31m"
+yellow = "\033[1;33m"
+skyblue = "\033[1;36m"
+blue = "\033[1;34m"
+lightblue = "\033[38;5;81m"
+white = "\033[1;37m"
+gh1 ="\x1b[38;5;166m"
+gh2 = "\x1b[38;5;167m"
+gh3 = "\x1b[38;5;168m"
+gh4 = "\x1b[38;5;169m"
+gh5  = "\x1b[38;5;170m"
+gh6 = "\x1b[38;5;171m"
+rb1 = "\x1b[38;5;34m"
+rb2 = "\x1b[38;5;35m"
+rb3 = "\x1b[38;5;36m"
+rb4 = "\x1b[38;5;37m"
+rb5 = "\x1b[38;5;38m"
+rb6 = "\x1b[38;5;39m"
+C1 = "\x1b[38;5;202m"
+C2 = "\x1b[38;5;203m"
+C3 = "\x1b[38;5;204m"
+C4 = "\x1b[38;5;205m"
+C5 = "\x1b[38;5;206m"
+C6 = "\x1b[38;5;207m"
+Q = "\x1b[38;5;118m"
+Q2 = "\x1b[38;5;119m"
+Q3 = "\x1b[38;5;120m"
+Q4 = "\x1b[38;5;121m"
+Q5 = "\x1b[38;5;122m"
+Q6 = "\x1b[38;5;123m"
+W11 = "\x1b[42;19m"
+BG ="\x1b[47;90m"
+RW= "\x1b[0;95m"
+Z1 ="\x1b[38;5;30m"
+B1 = "\x1b[38;5;34m"
+B2 = "\x1b[38;5;35m"
+B3 = "\x1b[38;5;36m"
+B4 = "\x1b[38;5;37m"
+B5= "\x1b[38;5;38m"
+B6 = "\x1b[38;5;39m"
 PURPLE ='\x1b[38;5;46m'
 RED = '\033[1;91m'
 WHITE = '\033[1;97m'
@@ -24,13 +81,13 @@ os.system("clear")
 os.system('git pull --quiet 2>/dev/null')
 #──────────────{ JOIN WHATSAPP GROUP }──────────────#
 os.system("clear")
-print(f'{R}[{G}•{R}]{G} JOIN WHATSAPP GROUP {RESET}')
+print(f'{R}[{hotpink}•{R}]{green2} JOIN WHATSAPP GROUP {RESET}')
 os.system('xdg-open https://chat.whatsapp.com/JBRMR1OyrxK2tsDlJUgh69')
 Tweak=platform.architecture()[0]
 if Tweak=="32bit":
     os.system("clear")
     __import__("createfb")
 elif Tweak=="64bit":
-    os.system("clear");exit(f"{R}[{G}•{R}]{G} SORRY 64BIT DEVICE NOT SUPPORTED")
+    os.system("clear");exit(f"{R}[{hotpink}•{R}]{R} SORRY 64BIT DEVICE NOT SUPPORTED")
     __import__("createfb")
 #──────────────{ END }──────────────#
